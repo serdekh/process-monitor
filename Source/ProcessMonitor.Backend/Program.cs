30a12b4eb78ad908f99c6b2da23d40a36d54d6c6
serdekh <mistersergey055@gmail.com>
2026-06-22 19:50:52 +0200

    Feature: Add ProcessMonitor.Backend Project
    This commit adds a basic C# Console application with the solution file
    alongside it. The ProcessMonitor.sln file holds a reference to
    ProcessMonitor.Backend.csproj file. The solution file is currently not
    configured for building the project. When the cli client gets included,
    the solution file will have to be reconfigured to build both projects.

---
+++ Source/ProcessMonitor.Backend/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ProcessMonitor.Backend;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello from the server!");
+    }
+}
