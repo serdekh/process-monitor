d0a57803795668029e84e32e1f54184d67e6d2da
serdekh <mistersergey055@gmail.com>
2026-06-23 19:33:44 +0200

    Add ProcessMonitor.Processing skeleton
    Add a primitive implementation for the event computations.
    Currently only consists of a single class-type. Consider adding an
    interface to abstract the behaviour out of the implementation for future
    testing.
    
    Type:
    
    EventMetricsEngine - class that handles the input events channel
                         and outputs computed metrics into a new one.
    
    Note:
    
    This type computes hardcoded metrics since the 'Collection' layer is not
    properly implemented. As soon as the events are correctly parsed, this
    class should be refactored.

---
+++ Source/ProcessMonitor.Backend/Processing/EventMetricsEngine.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Channels;
+
+using ProcessMonitor.Backend.Models;
+using ProcessMonitor.Backend.Collection;
+
+using ProcessMonitor.Contracts.Snapshots;
+
+namespace ProcessMonitor.Backend.Processing;
+
+public sealed class EventMetricsEngine 
+{
+    private ChannelReader<RawKernelEvent> _input;
+    private ChannelWriter<ProcessMetricsSnapshot> _output;
+
+    public EventMetricsEngine(
+        Channel<RawKernelEvent> input,
+        Channel<ProcessMetricsSnapshot> output)
+    {
+        _input = input.Reader;
+        _output = output.Writer;
+    }
+
+    // Note: Consier bringing interface similar to Cancellation layer
+    // for future testing and consistent hosting with other classes
+    public async Task RunAsync(CancellationToken ct)
+    {
+        // TODO: Unhardcode snapshot initialization
+        // TODO: Properly handle WaitToReadAsync exceptions
+        // TODO: Add logging when cancellation is requested
+        // TODO: Add timeQuantum delay support
+        while (await _input.WaitToReadAsync(ct))
+        {
+            var inputEvent = await _input.ReadAsync(ct);
+            
+            var snapshot = new ProcessMetricsSnapshot()
+            {
+                ProcessId = inputEvent.ProcessId,
+                ProcessName = "NOT IMPLEMENTED",
+                CpuUsage = 1234.567,
+                ThreadCount = 1,
+                TimestampUtc = DateTime.Now,
+                Threads = new List<ThreadMetricsSnapshot>()
+            };
+
+            await _output.WriteAsync(snapshot);
+        }
+    }
+}
