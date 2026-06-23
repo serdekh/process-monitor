bb0e9700667fdfbce69f1f1d84cdb74639d32530
serdekh <mistersergey055@gmail.com>
2026-06-23 19:04:18 +0200

    Add builder skeleton
    Add a generic 'IHost' object and configure it to add background
    services, asynchronous channels and a state singleton object. Consider
    that this should be moved into a separate class called
    'ProcessMonitorHostBuilder' to avoid polluting the entry point.

--- Source/ProcessMonitor.Backend/Program.cs
+++ Source/ProcessMonitor.Backend/Program.cs
@@ -1,11 +1,53 @@
-﻿using System;
+﻿using System.Threading.Tasks;
+using System.Threading.Channels;
+
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+using ProcessMonitor.Backend.State;
+using ProcessMonitor.Backend.Models;
+using ProcessMonitor.Backend.Hosting;
+using ProcessMonitor.Backend.Transport;
+using ProcessMonitor.Backend.Collection;
+using ProcessMonitor.Backend.Processing;
+using ProcessMonitor.Backend.Publishing;
+using ProcessMonitor.Backend.Serialization;
+
+using ProcessMonitor.Contracts.Snapshots;
 
 namespace ProcessMonitor.Backend;
 
 internal class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello from the server!");
+        var builder = Host.CreateApplicationBuilder(args);
+
+        builder.Services.AddSingleton<MonitoringSessionState>();
+        builder.Services.AddSingleton(Channel.CreateUnbounded<RawKernelEvent>());
+        builder.Services.AddSingleton(Channel.CreateUnbounded<ProcessMetricsSnapshot>());
+        builder.Services.AddSingleton<
+            IMessageSerializer, 
+            JsonMessageSerializer>();
+
+        builder.Services.AddSingleton<
+            ITelemetryTransport,
+            TelemetryTransport>();
+
+        builder.Services.AddSingleton<
+            IEventCollector,
+            EventStreamCollector>();
+
+        builder.Services.AddSingleton<EventMetricsEngine>();
+
+        builder.Services.AddSingleton<
+            IMetricsPublisher,
+            IPCMetricsPublisher>();
+
+        builder.Services.AddHostedService<CollectorHostedService>();
+        builder.Services.AddHostedService<EngineHostedService>();
+        builder.Services.AddHostedService<PublisherHostedService>();
+
+        await builder.Build().RunAsync();
     }
 }
