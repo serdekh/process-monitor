256c0b22a3d0461d1151fb4e93d71eab4ab35357
serdekh <mistersergey055@gmail.com>
2026-06-26 16:37:15 +0200

    Remove application builder code to different class
    Instead of controlling the application host builder directly from the
    Program.cs file, the code has been moved to
    'ProcessMonitor.Hosting.ProcessMonitorHostBuilder.cs' file. Now the
    entry point only creates the host objects and runs it.

--- Source/ProcessMonitor.Backend/Program.cs
+++ Source/ProcessMonitor.Backend/Program.cs
@@ -1,59 +1,19 @@
 ﻿using System.Threading.Tasks;
-using System.Threading.Channels;
 
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 
-using ProcessMonitor.Backend.State;
-using ProcessMonitor.Backend.Models;
 using ProcessMonitor.Backend.Hosting;
-using ProcessMonitor.Backend.Transport;
-using ProcessMonitor.Backend.Collection;
-using ProcessMonitor.Backend.Processing;
-using ProcessMonitor.Backend.Publishing;
-using ProcessMonitor.Backend.Serialization;
-
-using ProcessMonitor.Contracts.Snapshots;
 
 namespace ProcessMonitor.Backend;
 
 internal class Program
 {
     public static async Task Main(string[] args)
-    {
-        var builder = Host.CreateApplicationBuilder(args);
+    { 
+        var host = ProcessMonitorHostBuilder 
+            .Create(args)
+            .Build();
 
-        builder.Services.AddSingleton<MonitoringSessionState>();
-        builder.Services.AddSingleton(Channel.CreateUnbounded<RawKernelEvent>());
-        builder.Services.AddSingleton(Channel.CreateUnbounded<ProcessMetricsSnapshot>());
-        
-        builder.Logging.ClearProviders();
-        builder.Logging.AddConsole();
-        builder.Logging.AddDebug();
-
-        builder.Services.AddSingleton<
-            IMessageSerializer, 
-            JsonMessageSerializer>();
-
-        builder.Services.AddSingleton<
-            ITelemetryTransport,
-            TelemetryTransport>();
-
-        builder.Services.AddSingleton<
-            IEventCollector,
-            EventStreamCollector>();
-
-        builder.Services.AddSingleton<EventMetricsEngine>();
-
-        builder.Services.AddSingleton<
-            IMetricsPublisher,
-            IPCMetricsPublisher>();
-
-        builder.Services.AddHostedService<CollectorHostedService>();
-        builder.Services.AddHostedService<EngineHostedService>();
-        builder.Services.AddHostedService<PublisherHostedService>();
-
-        await builder.Build().RunAsync();
+        await host.RunAsync();
     }
 }
