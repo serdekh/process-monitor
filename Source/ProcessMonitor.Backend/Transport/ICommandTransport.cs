863ec4877a0ef9667a1f831d286647db27c38c14
serdekh <mistersergey055@gmail.com>
2026-06-27 22:19:55 +0200

    Add 'InitializeAsync' method
    Extend the original interface with a new method for initializing the
    pipe server as a necessity for the command controller class.

--- Source/ProcessMonitor.Backend/Transport/ICommandTransport.cs
+++ Source/ProcessMonitor.Backend/Transport/ICommandTransport.cs
@@ -5,7 +5,9 @@ namespace ProcessMonitor.Backend.Transport;
 
 public interface ICommandTransport
 {
-    Task<byte[]> ReceiveAsync(CancellationToken ct);
+    Task<byte[]?> ReceiveAsync(CancellationToken ct);
 
     Task SendAsync(byte[] data, CancellationToken ct);
+
+    Task InitializeAsync(CancellationToken ct);
 }
