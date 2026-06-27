00fc70f128d2307370a2c135cd64ea682587c5e0
serdekh <mistersergey055@gmail.com>
2026-06-27 22:15:47 +0200

    Implement a basic skeleton for named pipes based IPC
    Define several methods for reading, writing byte streams and initializing the pipe.
    Add private members for direct 'IPC' communication. Initialize the pipe
    server inside the constructor and wait for client connections in the corresponding
    method.

--- Source/ProcessMonitor.Backend/Transport/CommandTransport.cs
+++ Source/ProcessMonitor.Backend/Transport/CommandTransport.cs
@@ -1,34 +1,45 @@
 using System;
+using System.IO;
+using System.IO.Pipes;
 using System.Threading;
 using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
 
 namespace ProcessMonitor.Backend.Transport;
 
 public class CommandTransport : ICommandTransport
 {
-    public async Task<byte[]> ReceiveAsync(CancellationToken ct)
+    private readonly NamedPipeServerStream _pipe;
+    
+    private IPCProtocolReader _reader;
+    private IPCProtocolWriter _writer;
+
+    public CommandTransport()
     {
-        if (ct.IsCancellationRequested)
-        {
-            // TODO: Log operation cancellation
-        }
+        _pipe = new NamedPipeServerStream(
+            pipeName:                   "ProcessMonitor.Pipes.Commands",
+            direction:                  PipeDirection.InOut,
+            maxNumberOfServerInstances: 1,
+            transmissionMode:           PipeTransmissionMode.Byte,
+            options:                    PipeOptions.Asynchronous);
 
-        await Task.Delay(1000, ct);
-        var bytes = new byte[1] {123};
-
-        return bytes; 
+        _reader = new IPCProtocolReader(_pipe);
+        _writer = new IPCProtocolWriter(_pipe);
     }
 
-    public Task SendAsync(byte[] data, CancellationToken ct)
-    { 
-        if (ct.IsCancellationRequested)
-        {
-            // TODO: Log operation cancellation
-            return Task.CompletedTask;
-        }
+    public async Task InitializeAsync(CancellationToken ct)
+    {
+        await _pipe.WaitForConnectionAsync();
+    }
 
-        // TODO: Replace hardcoded log with proper logic
-        Console.WriteLine($"Sent {data.Length} bytes");
-        return Task.CompletedTask;
+    public async Task<byte[]?> ReceiveAsync(CancellationToken ct)
+    {
+        return await _reader.ReadAsync(ct);
+    }
+
+    public async Task SendAsync(byte[] data, CancellationToken ct)
+    {
+        await _writer.WriteAsync(data, ct);
     }
 }
