d9ce1aa1bb7b2be19fbb9a794d2430a52ae9b9ac
serdekh <mistersergey055@gmail.com>
2026-06-28 18:23:22 +0200

    Refactor initial implementation
    1: Add the 'TryCreateBackendProcess' method.
       Creates the backend process, catches all the possible errors,
       initializes a local '_process' field and returns a boolean
       indicating success or failure.
    
    2: Add the 'TryCreateClientStream' method
       Creates a named pipe client stream instance and connects it
       to the 'Commands' pipe. Catches all the errors, initializes the
       _client field and returns a boolean indicating success or failure.
    
    3: Remove the 'Write' method and replace it with a general 'WriteAsync'
       method that accepts a generic request object.
    
    4: Extract some logic from the 'ConnectAsync' method to the other
       methods mentioned in the '1' and '2' sections.
    
    5: Remove the reduntant 'BackendFileName' field since this information
       is already contained withtin the start info class.

--- Source/ProcessMonitor.CLI/Transport/CommandPipeClient.cs
+++ Source/ProcessMonitor.CLI/Transport/CommandPipeClient.cs
@@ -1,9 +1,15 @@
 using System;
 using System.IO;
 using System.IO.Pipes;
-using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using System.ComponentModel;
+
+// TODO: Extract the serialization code into a the contracts project
+// since it is being used by both the server and the client. Consider
+// renaming the contracts project to shared.
+using ProcessMonitor.Backend.Serialization;
 
 using ProcessMonitor.Contracts.Protocol;
 
@@ -11,11 +17,11 @@ namespace ProcessMonitor.CLI.Transport;
 
 public sealed class CommandPipeClient : IDisposable
 {
-    public string BackendFilePath { get; set; } 
-
     public ProcessStartInfo BackendStartInfo { get; set; }
 
     private NamedPipeClientStream? _client = null;
+
+    private IMessageSerializer _serializer;
 
     public bool IsConnected 
     { 
@@ -25,19 +31,21 @@ public sealed class CommandPipeClient : IDisposable
         } 
     }
 
-    public CommandPipeClient(string backendFilePath)
+    public CommandPipeClient(string backendFilePath, IMessageSerializer? serializer = null)
     {
-        BackendFilePath = backendFilePath;
-        BackendStartInfo = new ProcessStartInfo() 
+        BackendStartInfo = new ProcessStartInfo
         {
-            FileName = BackendFilePath,
+            FileName = backendFilePath,
             UseShellExecute = true
         };
+
+        _serializer = serializer is null ? new JsonMessageSerializer() : serializer;
     }
     
     public void CleanupConnection()
     {
         _client?.Dispose(); _client = null;
+        Console.WriteLine("procmon: info: The current client got disconnected.");
     }
 
     public void Dispose()
@@ -45,88 +53,112 @@ public sealed class CommandPipeClient : IDisposable
         CleanupConnection();
     }
 
-    // TODO: Refactor this code to properly behave in case of errors
-    public async Task Write()
+    public async Task WriteAsync(CommandRequest request)
     {
-        if (_client is null) return;
- 
-        for (int i = 0; i < 5; i++) 
+        if (_client is null)
         {
-            try
-            {
-                var message = "Hello from the client! IPC IS ACHIEVED!";
+            Console.WriteLine("procmon: error: Could not write a request. No connection was established.");
+            return;
+        }
+ 
+        try 
+        {
+            var requestBytes = _serializer.Serialize(request);
 
-                var messageBytes = System.Text.Encoding.UTF8.GetBytes(message);
+            var lengthBytes = BitConverter.GetBytes(requestBytes.Length);
 
-                var lengthBytes = BitConverter.GetBytes(messageBytes.Length);
+            await _client.WriteAsync(lengthBytes, 0, 4);
+        
+            await _client.WriteAsync(requestBytes, 0, requestBytes.Length);
 
-                await _client.WriteAsync(lengthBytes, 0, 4);
-
-                await _client.WriteAsync(messageBytes, 0, messageBytes.Length);
-
-                await _client.FlushAsync();
-            } 
-            catch (Exception)
-            {
-                Console.WriteLine("procmon: error: the server has disconnected or the pipe has been corrupted."); 
-            }
+            await _client.FlushAsync();
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("procmon: error: could not write a request to the `Commands` pipe.");
         }
     }
 
-    // TODO: Move code that creates a process into a separate class
-    public async Task ConnectAsync()
+    private bool TryCreateBackendProcess()
     {
-        if (_client is not null) CleanupConnection();
-
-        Console.WriteLine("procmon: info:  The previous connection is closed.");
-
-        try 
+        try
         {
             var process = Process.Start(BackendStartInfo);
-
+            
             if (process is null)
             {
                 Console.WriteLine("procmon: error: Could not start the backend process.");
-                return;
             }
 
-            Console.WriteLine("procmon: info:  Started the backend process.");
-
-            _client = new NamedPipeClientStream(
-                ".", 
-                "ProcessMonitor.Pipes.Commands", 
-                PipeDirection.InOut,
-                PipeOptions.Asynchronous);
-
-            if (_client is null) 
-            {
-                Console.WriteLine("procmon: error: Could not create the `Commands` pipe client.");
-                return;
-            }
-
-            Console.WriteLine("procmon: info:  Created the `Commands` pipe client.");
-
-            try 
-            {
-                await _client.ConnectAsync();
-                Console.WriteLine("procmon: info:  Connected to the `Commands` pipe");
-            }
-            catch (InvalidOperationException ) 
-            {
-                Console.WriteLine("procmon: error: Could not connect to `Commands` pipe: already connected.");
-                return;
-            }            
+            return process is null;
         }
         catch (Win32Exception)
         {
-            Console.WriteLine("procmon: error: System exception: could not open a file.");   
-            return;
-        }
-        catch (Exception e)
+            Console.WriteLine("procmon: error: The file was not found, access was denied or executable was corruputed.");
+        }       
+        catch (FileNotFoundException)
         {
-           Console.WriteLine(e);
-           Console.WriteLine("procmon: error: fatal");
-           return;
+            Console.WriteLine($"procmon: error: Could not find the file at {BackendStartInfo.FileName}.");
         }
+        catch (ObjectDisposedException)
+        {
+            Console.WriteLine("procmon: error: Could not start a process that has already been disposed.");
+        }
+        catch (ArgumentNullException)
+        {
+            Console.WriteLine("procmon: error: No process startup information was provided.");
+        }
+        catch (InvalidOperationException)
+	    {
+	        Console.WriteLine("procmon: error: No file name was provided or stream redirection failed.");
+	    }	 
+        catch (Exception)
+        {
+            Console.WriteLine("procmon: error: Unknown error.");
+        }
+
+        return false;
+    }
+ 
+    private bool TryCreateClientStream()
+    {
+        _client = new NamedPipeClientStream(
+            ".", 
+            "ProcessMonitor.Pipes.Commands", 
+            PipeDirection.InOut,
+            PipeOptions.Asynchronous);
+
+        if (_client is null) 
+        {
+            Console.WriteLine("procmon: error: Could not create the `Commands` pipe client.");
+            return false;
+        }
+
+        Console.WriteLine("procmon: info: Created the `Commands` pipe client.");
+        return true;
+     }
+   
+    public async Task<bool> ConnectAsync()
+    {
+        if (_client is not null) CleanupConnection();
+
+        if (!TryCreateBackendProcess()) return false; 
+
+        if (!TryCreateClientStream()) return false;
+
+        try 
+        { 
+            if (_client is null) return false;
+
+            await _client.ConnectAsync();
+                
+            Console.WriteLine("procmon: info:  Connected to the `Commands` pipe");
+        }
+        catch (InvalidOperationException) 
+        {
+            Console.WriteLine("procmon: error: Could not connect to `Commands` pipe: already connected.");
+        }            
+
+        return IsConnected;
     }
 }
