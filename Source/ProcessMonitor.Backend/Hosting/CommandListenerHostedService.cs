87e81c5e2a9f1989f4be82633171655fdd60b3ed
serdekh <mistersergey055@gmail.com>
2026-06-29 15:25:54 +0200

    Add logging

--- Source/ProcessMonitor.Backend/Hosting/CommandListenerHostedService.cs
+++ Source/ProcessMonitor.Backend/Hosting/CommandListenerHostedService.cs
@@ -2,6 +2,7 @@ using System.Threading;
 using System.Threading.Tasks;
 
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 using ProcessMonitor.Backend.Commands;
 
@@ -9,15 +10,26 @@ namespace ProcessMonitor.Backend.Hosting;
 
 public sealed class CommandListenerHostedService : BackgroundService
 {
+    private ILogger<CommandListenerHostedService> _logger;
+
     private readonly CommandController _controller;
 
-    public CommandListenerHostedService(CommandController controller)
+    public CommandListenerHostedService(
+        ILogger<CommandListenerHostedService> logger,
+        CommandController controller)
     {
+        _logger = logger;
         _controller = controller;
     }
 
     protected override async Task ExecuteAsync(CancellationToken ct)
     {
+        if (ct.IsCancellationRequested) return;
+
+        _logger.LogInformation("Command listening: Start.");
+
         await _controller.RunAsync(ct);
+
+        _logger.LogInformation("Command listening: Stop.");
     }
 }
