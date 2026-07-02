1837c6e16dfe830b8284b9eaedc1db23d0728247
serdekh <mistersergey055@gmail.com>
2026-07-02 20:21:42 +0200

    Move Process creation into a separate class
    Move the previous logic of handling the backend process into a separate
    class for making a clean separation of responsibilities and simplifying
    future work with the telemetry pipe.

---
+++ Source/ProcessMonitor.CLI/Common/BackendProcess.cs
@@ -0,0 +1,96 @@
+using System;
+using System.IO;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+namespace ProcessMonitor.CLI.Common;
+
+public sealed class BackendProcess : IDisposable
+{
+    private Process? _backend = null;
+
+    private Exception? _error = null;
+
+    private ProcessStartInfo _startInfo;
+
+    public string Path 
+    { 
+        get
+        {
+            return _startInfo.FileName;
+        } 
+    } 
+
+    public bool IsCreated 
+    { 
+        get
+        {
+            return _backend is not null; 
+        }
+    }
+
+    public bool HasError
+    {
+        get
+        {
+            return _error is not null;
+        }
+    }
+
+    public BackendProcess(string path)
+    {
+        _startInfo = new ProcessStartInfo
+        {
+            FileName = path,
+            UseShellExecute = true
+        };
+    }
+
+    public bool Create()
+    {
+        if (_backend is not null) return true;
+
+        _error = null;
+
+        try
+        {
+            _backend = Process.Start(_startInfo);
+        }
+        catch (Exception ex)
+        {
+            _error = ex;
+        }
+
+        return _error is null;
+    }
+
+    public string GetErrorString()
+    {
+        if (HasError) return "No error";
+
+        switch (_error)
+        {
+            case Win32Exception:            return "The file was not found, access was denied or executable was corruputed";
+            case FileNotFoundException:     return $"The file {_startInfo.FileName} was not found";
+            case ObjectDisposedException:   return "Could not start a backend process that has been disposed";
+            case ArgumentNullException:     return "No process start-up information was provided";
+            case InvalidOperationException: return "No file name was provided or stream redirection failed";
+            
+            default: return "Unknown error";     
+        }
+    }
+
+    public void Kill()
+    {
+        if (_backend is null) return;
+
+        _backend.Kill();
+        _backend.WaitForExit();
+    }
+
+    public void Dispose()
+    {
+        this.Kill();
+    }
+}
