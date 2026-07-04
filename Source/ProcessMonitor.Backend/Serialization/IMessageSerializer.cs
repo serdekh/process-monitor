5c175e0ebd13a81cb4a35d2ef8d4eb976a87ad84
serdekh <mistersergey055@gmail.com>
2026-07-04 12:32:05 +0200

    Delete the 'Serialization' layer
    Since the 'Serialization' layer is moved to the 'Contracts' project,
    there is no need to keep it here.

--- Source/ProcessMonitor.Backend/Serialization/IMessageSerializer.cs
+++
@@ -1,8 +0,0 @@
-namespace ProcessMonitor.Backend.Serialization;
-
-public interface IMessageSerializer
-{
-    public byte[] Serialize<T>(T message);
-
-    public T? Deserialize<T>(byte[] data);
-}
