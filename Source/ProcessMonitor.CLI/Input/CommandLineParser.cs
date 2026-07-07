728fd533206779cf95ea0fb27e2b62770feacac8
serdekh <mistersergey055@gmail.com>
2026-07-07 08:51:19 +0200

    Implement a proper flag parsing mechanism
    Instead of checking whether a string is equal to a certain flag, the
    class now supports mapping an input flag to its handler method that also
    mutates the value of the args index for advancing the parsing logic.
    
    Itwas decided that the redifinition is an error and once an error occurs,
    the parsing is completely stopped and the application terminates.
    
    A new flag was added called '--path' that accepts a path to the backend
    process. Once a user runs the 'create' command, this filepath gets used
    to instantiate the backend process. Obviously, it's inconvinient but
    it's better than just hardcoding it like I did before. Now whoever
    decides to use the app, can specify the path to the backend themselves.
    
    Consider adding a convenient configuration manager for loading a json
    file with the predefined settings to avoid passing the '--path' flag
    every time.
    
    Also consider a more sophisticated logic when the '--path' is not
    provided and trying to find the .dll file by going to the binary folder
    of the project.

--- Source/ProcessMonitor.CLI/Input/CommandLineParser.cs
+++ Source/ProcessMonitor.CLI/Input/CommandLineParser.cs
@@ -1,37 +1,135 @@
 using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace ProcessMonitor.CLI.Input;
 
 public sealed class CommandLineParser
 {
-    public string ErrorMessage { get; private set; } = string.Empty;
+    public readonly Dictionary<string, object?> Flags = [];
 
-    public int? ParseProcessId(string[] args)
+    private delegate bool FlagHandler(string[] args, ref int args_i);
+
+    private Dictionary<string, FlagHandler> _handlers;
+
+    private string[]? _args;
+
+    public List<string> ErrorMessages { get; private set; } = [];
+
+    public bool HasError
     {
-        ErrorMessage = string.Empty;
+        get
+        {
+            return ErrorMessages.Count > 0;
+        }
+    }
+
+    public CommandLineParser(string[]? args = null)
+    {
+        _handlers = new Dictionary<string, FlagHandler>
+        {
+            ["--pid"] = ParsePid,
+            ["--path"] = ParsePath,
+        };
+
+        _args = args;
+    }
+
+    public void PrintErrors()
+    {
+        foreach (var error in ErrorMessages)
+        {
+            Console.WriteLine($"procmon: error: {error}.");
+        }
+    }
+
+    private bool ParsePath(string[] args, ref int args_i)
+    {
+        var flag = args[args_i];
+
+        if (Flags.ContainsKey(flag))
+        {
+            ErrorMessages.Add($"Redefinition of the '{flag}' flag");
+            return false;
+        }
+
+        if (args_i + 1 >= args.Length)
+        {
+            ErrorMessages.Add($"Not enough arguments for the '{flag}' flag.\nUsage: {flag} <file-path>");
+            return false;
+        }
+
+        if (!File.Exists(args[args_i + 1]))
+        {
+            ErrorMessages.Add($"Could not find a file at '{args[args_i + 1]}' for the '{flag}' flag\nUsage: --{flag} <file-path>");
+            return false;
+        }
+
+        Flags.Add(flag, args[args_i + 1]);
+
+        args_i++;
+
+        return true;
+    }
+
+    private bool ParsePid(string[] args, ref int args_i)
+    {
+        var flag = args[args_i];
+
+        if (Flags.ContainsKey(flag))
+        {
+            ErrorMessages.Add($"Redefinition of the '{flag}' flag");
+            return false;
+        }
+
+        if (args_i + 1 >= args.Length)
+        {
+            ErrorMessages.Add($"Not enough arguments for the '{flag}' flag.\nUsage: --pid <int>");
+            return false;
+        }
+
+        if (!int.TryParse(args[args_i + 1], out int pid))
+        {
+            ErrorMessages.Add($"Could convert the input argument ('{args[args_i + 1]}') of the '{flag}' flag into an integer\nUsage: --pid <int>");
+            return false;
+        }
+
+        args_i++;
+
+        Flags.Add(flag, pid);
+
+        return true;
+    }
+
+    public bool Parse(string[] args)
+    {
+        ErrorMessages.Clear();
 
         for (int i = 0; i < args.Length; i++)
         {
-            if (args[i].Equals("--pid"))
+            if (!_handlers.TryGetValue(args[i], out FlagHandler? flagHandler))
             {
-                if (i + 1 >= args.Length) 
-                {
-                    ErrorMessage = "The `--pid` flag is missing its argument.\nUsage: `--pid <process-id>`.";                   
-                    return null;
-                }
+                ErrorMessages.Add($"Could not parse the input flag: '{args[i]}'");
+                return false;
+            }
 
-                var inputArgument = args[i + 1];
-
-                if (!Int32.TryParse(inputArgument, out int processId))
-                {
-                    ErrorMessage = $"The argument for the `--pid` flag (`{inputArgument}`) is not an integer.";
-                    return null;
-                }
-
-                return processId;
+            if (!flagHandler(args, ref i))
+            {
+                return false;
             }
         }
 
-        return null;
+        return true;
+    }
+
+    public bool Parse()
+    {
+        if (_args is null) 
+        {
+            ErrorMessages.Add("No input args were provided in the constructor");
+            return false;
+        }
+
+        return Parse(_args);
     }
 }
