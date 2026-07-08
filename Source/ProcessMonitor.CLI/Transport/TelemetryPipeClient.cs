dc7c5845c3b0745ede0c73e8f7f90b81a5ad1f97
serdekh <mistersergey055@gmail.com>
2026-07-08 05:57:55 +0200

    Add ReadAsync method
    Add a new method for connecting to the telemetry pipe and continiously
    read from there until the pipe is disposed.
    
    Consider adding new methods to the Serialization layer to avoid constant
    prefixed message build and instead let another method automatically
    build the serializaed prefix message

--- Source/ProcessMonitor.CLI/Transport/TelemetryPipeClient.cs
+++ Source/ProcessMonitor.CLI/Transport/TelemetryPipeClient.cs
@@ -1,12 +1,19 @@
 using System;
+using System.IO;
+using System.Text;
 using System.IO.Pipes;
 using System.Threading;
 using System.Diagnostics;
+using System.Buffers.Binary;
 using System.Threading.Tasks;
 
 using Microsoft.Extensions.Logging;
 
 using ProcessMonitor.CLI.Common;
+
+using ProcessMonitor.Shared.Protocol;
+using ProcessMonitor.Shared.Snapshots;
+using ProcessMonitor.Shared.Serialization;
 
 namespace ProcessMonitor.CLI.Transport;
 
@@ -18,14 +25,18 @@ public sealed class TelemetryPipeClient : IAsyncDisposable
 
     private ILogger<TelemetryPipeClient> _logger;
 
+    private readonly IMessageSerializer _serializer;
+
     public bool IsConnected => _backend.IsRunning && (_telemetryClient?.IsConnected ?? false);
 
     public TelemetryPipeClient(
         BackendProcess backend, 
-        ILogger<TelemetryPipeClient> logger)
+        ILogger<TelemetryPipeClient> logger,
+        IMessageSerializer serializer)
     {
         _backend = backend;
         _logger = logger;
+        _serializer = serializer;
 
         _backend.AddOnExitHandler(async (sender, e) =>
         {
@@ -80,7 +91,7 @@ public sealed class TelemetryPipeClient : IAsyncDisposable
     public async Task<bool> TryConnectAsync(CancellationToken ct)
     {
         if (IsConnected) return true;
-        
+
         if (ct.IsCancellationRequested)
         {
             _logger.LogInformation("[Telemetry]: Could not connect to the telemetry pipe: cancellation requested.");    
@@ -114,6 +125,103 @@ public sealed class TelemetryPipeClient : IAsyncDisposable
         catch (Exception ex)
         {
             _logger.LogError("[Telemetry]: Failed to connect to the 'Telemetry' pipe: {}.", ex.Message);
+        }
+
+        return true;
+    }
+
+    public async Task<bool> ReadAsync(CancellationToken ct)
+    {
+        if (ct.IsCancellationRequested)
+        {
+            _logger.LogError("[Telemetry]: Could not start reading a telemetry message: cancellation requested.");    
+            return false;
+        }
+
+        if (!IsConnected)
+        {
+            _logger.LogError("[Telemetry]: Failed to read a telemetry message: no connection.");
+            return false;    
+        }
+
+        Debug.Assert(_telemetryClient is not null, "_telemetryClient is expected to be non-null in here. If you see this message, then there is a bug");
+
+        var prefixBuffer = new byte[4];
+
+        while (!ct.IsCancellationRequested)
+        {
+            int prefixBytesRead;
+
+            try
+            {
+                if (_telemetryClient is null)
+                {
+                    _logger.LogError("[Telemetry]: Telemetry client stream object is null.");
+                    await CleanupConnectionAsync();
+                    return false;
+                }
+
+                prefixBytesRead = await _telemetryClient.ReadAsync(prefixBuffer.AsMemory(0, 4), ct);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("[Telemetry]: Failed to read a telemetry message: the size prefix has been corruputed: {}", ex.Message);
+                await CleanupConnectionAsync();
+                return false;
+            }
+
+            if (prefixBytesRead == 0)
+            {
+                _logger.LogInformation("[Telemetry]: Could not read a telemetry message: reached the end of the pipe.");
+                await CleanupConnectionAsync();
+                return true;
+            }
+
+            int rawMessageLength = BinaryPrimitives.ReadInt32LittleEndian(prefixBuffer);
+            if (rawMessageLength <= 0) continue;
+
+            var rawMessage = new byte[rawMessageLength];
+            
+            for (int totalBytesRead = 0; totalBytesRead < rawMessageLength;)
+            {
+                try
+                {
+                    int read = await _telemetryClient.ReadAsync(rawMessage.AsMemory(totalBytesRead, rawMessageLength - totalBytesRead), ct);
+                    
+                    if (read == 0) throw new EndOfStreamException("The backend process has exited");
+
+                    totalBytesRead += read;
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError("[Telemetry]: Failed to read a telemetry message: {}.", ex.Message);
+                    await CleanupConnectionAsync();
+                    return false;
+                }
+            }
+
+            MessageEnvelope<ProcessMetricsSnapshot>? snapshot;
+            
+            try
+            {
+                snapshot = _serializer.Deserialize<MessageEnvelope<ProcessMetricsSnapshot>>(rawMessage);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("[Telemetry]: Failed to read a telemetry message: {}.", ex.Message);
+                await CleanupConnectionAsync();
+                return false;
+            }
+
+            if (snapshot is null)
+            {
+                _logger.LogError("[Telemetry]: Failed to read a telemetry message: could not deserialize the message into the metrics snapshot.");
+                await CleanupConnectionAsync();
+                return false;
+            }
+
+            // temp
+            Console.WriteLine(snapshot.Payload.ToString());
         }
 
         return true;
