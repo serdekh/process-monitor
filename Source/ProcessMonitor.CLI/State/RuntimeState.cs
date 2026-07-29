d6d4682e88bbf5c6f0baa5c3fa61ff55ed3726dc
serdekh <mistersergey055@gmail.com>
2026-07-29 20:56:40 +0200

    Remove since ClientApplicationState serves this purpose now

--- Source/ProcessMonitor.CLI/State/RuntimeState.cs
+++
@@ -1,8 +0,0 @@
-namespace ProcessMonitor.CLI.State;
-
-public sealed class RuntimeState
-{
-    public int TargetPid { get; set; } = 0;
-
-    public string BackendProcessFilePath { get; set; } = string.Empty;
-}
