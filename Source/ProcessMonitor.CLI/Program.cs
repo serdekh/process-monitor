9fcc3b4bf239c56ca95b56f8d91e5f86e5e0c665
serdekh <mistersergey055@gmail.com>
2026-08-01 19:35:18 +0200

    Add privilege check and update args parsing validation

--- Source/ProcessMonitor.CLI/Program.cs
+++ Source/ProcessMonitor.CLI/Program.cs
@@ -2,11 +2,8 @@
 using System.Threading.Tasks;
 
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 
 using ProcessMonitor.CLI.Hosting;
-using ProcessMonitor.CLI.Input.Args;
-using ProcessMonitor.Shared.Client.State;
 
 namespace ProcessMonitor.CLI;
 
@@ -14,40 +11,19 @@ internal class Program
 {
     public static async Task Main(string[] args)
     {
-        var argsParser = new CommandLineParser(args);
-
-        if (!argsParser.Parse())
+        if (!Environment.IsPrivilegedProcess)
         {
-            argsParser.PrintErrors();
+            Console.WriteLine($"[ProcessMonitor]: error: Could only run as administrator.");
             return;
         }
 
-        // TODO: Replace mannual '--path' flag insertion with a custom config file
-        if (!argsParser.Flags.TryGetValue("--path", out object? value))
+        (var builder, var parsingException) = CLIHostBuilder.Create(args);
+
+        if (parsingException is not null)
         {
-            Console.WriteLine("procmon: error: The filepath to the backend process was not specified.\nProvide the '--path <file-path>' flag in the command line arguments.");
+            Console.WriteLine($"[ProcessMonitor]: error: Could not parse input arguments: {parsingException.Message}");
             return;
         }
-
-        if (value is not string path) return;
-
-        var builder = CLIHostBuilder 
-            .Create(args);
-
-        if (!argsParser.Flags.TryGetValue("--pid", out object? flag))
-        {
-            flag = 0;
-        }
-
-        int pid = flag is null ? 0 : (int)flag;
-
-        // NOTE: Consider replacing this code with a refactorred logic inside the ConsoleInputReader class
-        // so that we don't explicitly inject this path string. For example, add a new hosted service that
-        // manages cli arguments and exposes them to the other services inside the ConfigureServices method. 
-        builder.Services.Configure<ClientApplicationOptions>(options =>
-        {
-            options.BackendPath = path;
-        });
 
         await builder.Build().RunAsync();
     }
