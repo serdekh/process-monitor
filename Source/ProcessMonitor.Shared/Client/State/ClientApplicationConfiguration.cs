8f228b13c2c665fb1c83113afc18c44ba7568307
serdekh <mistersergey055@gmail.com>
2026-08-01 19:36:45 +0200

    Move configuration settings to a separate class

--- Source/ProcessMonitor.Shared/Client/State/ClientApplicationConfiguration.cs
+++ Source/ProcessMonitor.Shared/Client/State/ClientApplicationConfiguration.cs
@@ -3,5 +3,5 @@ public sealed class ClientApplicationConfiguration
 {
     public int? ProcessId { get; set; } = null;
 
-    public string? ServerFilepath { get; set; } = null;
+    public string ServerFilepath { get; set; } = string.Empty;
 }
