9c366c7ea1f4c06553c4306e3d916bdf86ec96e6
serdekh <mistersergey055@gmail.com>
2026-08-05 19:42:53 +0200

    Add a blank WPF experimental frontend

--- Source/ProcessMonitor.WPF_Experimental/MainWindow.xaml.cs
+++ Source/ProcessMonitor.WPF_Experimental/MainWindow.xaml.cs
@@ -4,8 +4,26 @@ namespace ProcessMonitor.WPF_Experimental;
 
 public partial class MainWindow : Window
 {
+    public bool RunButtonState { get; private set; }
+
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void Run_Click(object sender, RoutedEventArgs e)
+    {
+        if (RunButtonState)
+        {
+            MainLabel.Text = "Stopped";
+            Run.Content = "Run";
+        }
+        else
+        {
+            MainLabel.Text = "Running";
+            Run.Content = "Stop";
+        }
+
+        RunButtonState = !RunButtonState;
     }
 }
