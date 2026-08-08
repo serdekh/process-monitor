0c68276d3ab4162629f7cbf67e853d2342788063
serdekh <mistersergey055@gmail.com>
2026-08-08 17:51:12 +0200

    Add global states for tracking mode updates

---
+++ Source/WPF_Experimental/Client/State/ApplicationMode.cs
@@ -0,0 +1,22 @@
+﻿namespace WPF_Experimental.Client.State;
+
+public enum ApplicationMode
+{
+    Startup,
+    Settings,
+    Running
+}
+
+public static class ApplicationModeExtensions
+{
+    public static string AsString(this ApplicationMode mode)
+    {
+        return mode switch
+        {
+            ApplicationMode.Startup => "Startup",
+            ApplicationMode.Settings => "Settings",
+            ApplicationMode.Running => "Running",
+            _ => "Unknown"
+        };       
+    }
+}
---
+++ Source/WPF_Experimental/Client/State/ApplicationState.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WPF_Experimental.Client.State;
+
+public sealed class ApplicationState : INotifyPropertyChanged
+{
+    public static ApplicationState Instance { get; } = new ApplicationState();
+
+    private ApplicationState() { }
+
+    public ApplicationMode PreviousMode { get; private set; }
+
+    private ApplicationMode _currentMode;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public string CurrentModeAsString => CurrentMode.AsString();
+
+    public ApplicationMode CurrentMode
+    {
+        get { return _currentMode; }
+        set
+        {
+            if (_currentMode == value) return;
+
+            PreviousMode = CurrentMode;
+            _currentMode = value;
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(CurrentModeAsString));
+        }
+    }
+
+    private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
