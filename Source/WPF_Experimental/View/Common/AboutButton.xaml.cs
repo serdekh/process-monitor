8259f26ef374e8545c3f8f550deafaccc45708dc
serdekh <mistersergey055@gmail.com>
2026-08-11 18:48:31 +0200

    Add user controls for mode '?' and '⚙' buttons

---
+++ Source/WPF_Experimental/View/Common/AboutButton.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace WPF_Experimental.View.Common;
+
+public partial class AboutButton : UserControl
+{
+    public AboutButton()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Source/WPF_Experimental/View/Common/UndoSwitchButton.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using WPF_Experimental.Client.State;
+
+namespace WPF_Experimental.View.Common;
+
+public partial class UndoSwitchButton : UserControl
+{
+    public UndoSwitchButton()
+    {
+        InitializeComponent();
+    }
+
+    private void UndoSwitchModeButton_Click(object sender, RoutedEventArgs e)
+    {
+        ApplicationState.Instance.CurrentMode = ApplicationState.Instance.PreviousMode;
+    }
+}
+
