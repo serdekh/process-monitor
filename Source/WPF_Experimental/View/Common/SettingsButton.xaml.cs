b9f2b194d5da7fe5d7c4957c0030936694a1f993
serdekh <mistersergey055@gmail.com>
2026-08-11 18:45:36 +0200

    Add click event handler

--- Source/WPF_Experimental/View/Common/SettingsButton.xaml.cs
+++ Source/WPF_Experimental/View/Common/SettingsButton.xaml.cs
@@ -1,10 +1,4 @@
-﻿using ProcessMonitor.Shared.Client.State;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 
 using WPF_Experimental.Client.State;
