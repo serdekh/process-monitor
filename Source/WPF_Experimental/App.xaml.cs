8bc07938d8b03054640608558d55a7ecdee3ae44
serdekh <mistersergey055@gmail.com>
2026-08-11 18:43:34 +0200

    Add IHost instance

--- Source/WPF_Experimental/App.xaml.cs
+++ Source/WPF_Experimental/App.xaml.cs
@@ -1,6 +1,18 @@
 ﻿using System.Windows;
+using ProcessMonitor.Shared.Client.Hosting;
 
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
 
+        var builder = new ClientHostBuilder(e.Args);
+
+        builder.UseCore();
+
+        builder.Build();
+
+        builder.RunAsync();
+    }
 }
