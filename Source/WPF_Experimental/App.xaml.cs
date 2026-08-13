88aa5b800863fcc0c08cd8d7414c93bf064a1b34
serdekh <mistersergey055@gmail.com>
2026-08-13 19:48:13 +0200

    Create a static singleton instance for runtime state

--- Source/WPF_Experimental/App.xaml.cs
+++ Source/WPF_Experimental/App.xaml.cs
@@ -1,18 +1,25 @@
 ﻿using System.Windows;
-using ProcessMonitor.Shared.Client.Hosting;
+using Microsoft.Extensions.Options;
+using ProcessMonitor.Shared.Client.State;
+using ProcessMonitor.Shared.Serialization;
+using ProcessMonitor.Shared.Transport.Framing;
+
+namespace WPF_Experimental;
 
 public partial class App : Application
 {
-    protected override void OnStartup(StartupEventArgs e)
-    {
-        base.OnStartup(e);
+    private static readonly ClientApplicationState _runtimeState = new
+    (
+        new FrameWriter(),
+        new FrameReader(),
+        new JsonMessageSerializer(),
+        // TODO: Unhardcode these constants via the settings mode configurations
+        Options.Create(new ClientApplicationConfiguration 
+        { 
+            ProcessId = 0, 
+            ServerFilepath = "\"C:\\Users\\Serhii\\repos\\process-monitor\\Source\\ProcessMonitor.Backend\\bin\\Debug\\net9.0\\ProcessMonitor.Backend.exe\""
+        })
+    );
 
-        var builder = new ClientHostBuilder(e.Args);
-
-        builder.UseCore();
-
-        builder.Build();
-
-        builder.RunAsync();
-    }
+    public static ClientApplicationState RuntimeState => _runtimeState;
 }
