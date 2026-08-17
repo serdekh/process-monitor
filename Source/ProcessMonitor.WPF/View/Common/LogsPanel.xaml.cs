c89a1c0259d09d6f37af49c0d79d2dcb103cee89
serdekh <mistersergey055@gmail.com>
2026-08-17 20:04:06 +0200

    Add 'LogsPanel' user control

---
+++ Source/ProcessMonitor.WPF/View/Common/LogsPanel.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace ProcessMonitor.WPF.View.Common;
+
+public partial class LogsPanel : UserControl
+{
+    public LogsPanel()
+    {
+        InitializeComponent();
+    }
+}
