9e429d5dd30e78426614ef41dee68bd488b6a99b
serdekh <mistersergey055@gmail.com>
2026-08-17 21:08:11 +0200

    Add 'RunButton' user control & Add it to StartupMode

---
+++ Source/ProcessMonitor.WPF/View/Modes/Startup/RunButton.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace ProcessMonitor.WPF.View.Modes.Startup;
+
+public partial class RunButton : UserControl
+{
+    public RunButton()
+    {
+        InitializeComponent();
+    }
+}
