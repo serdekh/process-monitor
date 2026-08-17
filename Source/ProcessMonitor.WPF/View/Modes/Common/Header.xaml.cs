ecd843a3161ee9b30fd690c00b95d49235c90a88
serdekh <mistersergey055@gmail.com>
2026-08-17 20:15:16 +0200

    Add 'Header' user control

---
+++ Source/ProcessMonitor.WPF/View/Modes/Common/Header.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace ProcessMonitor.WPF.View.Modes.Common;
+
+public partial class Header : UserControl
+{
+    public Header()
+    {
+        InitializeComponent();
+    }
+}
