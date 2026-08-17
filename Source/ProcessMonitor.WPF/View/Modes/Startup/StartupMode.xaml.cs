95e9cd4c02c0ab6710143a24e56a4c2188a799c6
serdekh <mistersergey055@gmail.com>
2026-08-17 20:49:38 +0200

    Add 'StartupMode' user control

---
+++ Source/ProcessMonitor.WPF/View/Modes/Startup/StartupMode.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace ProcessMonitor.WPF.View.Modes.Startup;
+
+public partial class StartupMode : UserControl
+{
+    public StartupMode()
+    {
+        InitializeComponent();
+    }
+}
