87387f8337e128cdcd7ff128e4921433df0400e2
serdekh <mistersergey055@gmail.com>
2026-08-17 21:33:15 +0200

    Add 'CPUUsageBar' & 'RunningMode' controls

---
+++ Source/ProcessMonitor.WPF/View/Modes/Running/CPUUsageBar.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace ProcessMonitor.WPF.View.Modes.Running;
+
+public partial class CPUUsageBar : UserControl
+{
+    public CPUUsageBar()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Source/ProcessMonitor.WPF/View/Modes/Running/RunningMode.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace ProcessMonitor.WPF.View.Modes.Running;
+
+public partial class RunningMode : UserControl
+{
+    public RunningMode()
+    {
+        InitializeComponent();
+    }
+}
