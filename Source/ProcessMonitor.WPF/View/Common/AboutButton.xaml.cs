5cb0e469ef33f91d856f13852feb640b35b2cd97
serdekh <mistersergey055@gmail.com>
2026-08-17 19:32:00 +0200

    Add 'AboutButton' user control

---
+++ Source/ProcessMonitor.WPF/View/Common/AboutButton.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace ProcessMonitor.WPF.View.Common;
+
+public partial class AboutButton : UserControl
+{
+    public AboutButton()
+    {
+        InitializeComponent();
+    }
+}
