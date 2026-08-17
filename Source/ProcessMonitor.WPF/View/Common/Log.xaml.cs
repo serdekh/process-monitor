79bba9dbd08b9432f063b18eb3b4b3566ae7cd96
serdekh <mistersergey055@gmail.com>
2026-08-17 20:03:28 +0200

    Add 'Log' user control

---
+++ Source/ProcessMonitor.WPF/View/Common/Log.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace ProcessMonitor.WPF.View.Common;
+
+public partial class Log : UserControl
+{
+    public Log()
+    {
+        InitializeComponent();
+    }
+}
