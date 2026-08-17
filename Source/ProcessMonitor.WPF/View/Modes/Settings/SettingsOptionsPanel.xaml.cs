949753e27e3624f6fd6be9d23ea9e6b1ad315690
serdekh <mistersergey055@gmail.com>
2026-08-17 22:21:37 +0200

    Add 'SettingsOptionsPanel' user control

---
+++ Source/ProcessMonitor.WPF/View/Modes/Settings/SettingsOptionsPanel.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace ProcessMonitor.WPF.View.Modes.Settings;
+
+public partial class SettingsOptionsPanel : UserControl
+{
+    public SettingsOptionsPanel()
+    {
+        InitializeComponent();
+    }
+}
