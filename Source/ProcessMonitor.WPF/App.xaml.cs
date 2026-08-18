f543fb7068f33b8d6d3543acaa5a948e3ef9fb70
serdekh <mistersergey055@gmail.com>
2026-08-18 15:48:56 +0200

    Add input args parsing

--- Source/ProcessMonitor.WPF/App.xaml.cs
+++ Source/ProcessMonitor.WPF/App.xaml.cs
@@ -1,7 +1,33 @@
-﻿using System.Windows;
+﻿using ProcessMonitor.Shared.Client.Input.Args;
+using ProcessMonitor.WPF.State;
+
+using System.Windows;
 
 namespace ProcessMonitor.WPF;
 
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        var argsParser = new ArgsParser();
+
+        var parsingException = argsParser.Parse(e.Args);
+
+        if (parsingException != null)
+        {
+            MessageBox.Show(parsingException.Message, "error", MessageBoxButton.OK, MessageBoxImage.Error);
+            return;
+        }
+
+        GlobalState.Instance.Runtime.Backend.Path = argsParser.Configuration.ServerFilepath;
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        base.OnExit(e);
+
+        GlobalState.Instance.Runtime.Cleanup();
+    }
 }
