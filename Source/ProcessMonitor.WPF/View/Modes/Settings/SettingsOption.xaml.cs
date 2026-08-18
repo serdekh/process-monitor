97942757303ad609561eb43127c35dcacc905624
serdekh <mistersergey055@gmail.com>
2026-08-18 17:35:22 +0200

    Add data binding for setting options

--- Source/ProcessMonitor.WPF/View/Modes/Settings/SettingsOption.xaml.cs
+++ Source/ProcessMonitor.WPF/View/Modes/Settings/SettingsOption.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace ProcessMonitor.WPF.View.Modes.Settings;
 
@@ -7,5 +8,23 @@ public partial class SettingsOption : UserControl
     public SettingsOption()
     {
         InitializeComponent();
+    }
+
+    public static readonly DependencyProperty PropertyNameProperty =
+        DependencyProperty.Register("PropertyName", typeof(string), typeof(SettingsOption), new PropertyMetadata());
+
+    public static readonly DependencyProperty PropertyValueProperty =
+        DependencyProperty.Register("PropertyValue", typeof(string), typeof(SettingsOption), new PropertyMetadata());
+
+    public string PropertyName
+    {
+        get => (string)GetValue(PropertyNameProperty);
+        set => SetValue(PropertyNameProperty, value);
+    }
+
+    public string PropertyValue
+    {
+        get => (string)GetValue(PropertyValueProperty);
+        set => SetValue(PropertyValueProperty, value);
     }
 }
