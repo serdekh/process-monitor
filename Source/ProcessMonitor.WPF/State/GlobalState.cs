6de880861dc43b915c72f4a87631e927fba5ec10
serdekh <mistersergey055@gmail.com>
2026-08-18 14:44:00 +0200

    Add basic implementation for runtime state

---
+++ Source/ProcessMonitor.WPF/State/GlobalState.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Extensions.Options;
+
+using ProcessMonitor.Shared.Client.State;
+using ProcessMonitor.Shared.Serialization;
+using ProcessMonitor.Shared.Snapshots;
+using ProcessMonitor.Shared.Transport.Framing;
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ProcessMonitor.WPF.State;
+
+public sealed class GlobalState : INotifyPropertyChanged
+{
+    public static readonly GlobalState Instance = new();
+
+    public ModeState PreviousMode { get; private set; }
+
+    private ModeState _currentMode = ModeState.Startup;
+
+    public ModeState CurrentMode 
+    {
+        get { return _currentMode; }
+        set
+        {
+            if (value == _currentMode) return;
+
+            PreviousMode = _currentMode;
+            _currentMode = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private readonly ClientApplicationState _runtime = new
+    (
+        new FrameWriter(),
+        new FrameReader(),
+        new JsonMessageSerializer(),
+        Options.Create(new ClientApplicationConfiguration())
+    );
+
+    public ClientApplicationState Runtime => _runtime;
+
+    public ProcessMetricsSnapshot? LatestSnapshot 
+    {
+        get { return _runtime.LatestSnapshot; } 
+        set { _runtime.LatestSnapshot = value; OnPropertyChanged(); } 
+    }
+
+    private uint _latestRequestId = 0;
+
+    public uint LatestRequestId
+    {
+        get 
+        { 
+            var temp = _latestRequestId; _latestRequestId++;
+            OnPropertyChanged();
+            return temp;
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public void OnPropertyChanged([CallerMemberName] string propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ Source/ProcessMonitor.WPF/State/ModeState.cs
@@ -0,0 +1,22 @@
+﻿namespace ProcessMonitor.WPF.State;
+
+public enum ModeState
+{
+    Startup,
+    Settings,
+    Running
+}
+
+public static class ModeStateExtensions
+{
+    public static string ToString(this ModeState state)
+    {
+        return state switch
+        {
+            ModeState.Startup => "Startup",
+            ModeState.Settings => "Settings",
+            ModeState.Running => "Running",
+            _ => "Unknown"
+        };
+    }
+}
