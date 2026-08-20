eefac39539777319d8085a4d01a0fe71404cf99a
serdekh <mistersergey055@gmail.com>
2026-08-20 20:01:21 +0200

    Add custom instances of SettingsOption control

---
+++ Source/ProcessMonitor.WPF/View/Modes/Settings/BackendFilePathOption.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace ProcessMonitor.WPF.View.Modes.Settings;
+
+public partial class BackendFilePathOption : UserControl
+{
+    public BackendFilePathOption()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Source/ProcessMonitor.WPF/View/Modes/Settings/TargetPIDOption.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace ProcessMonitor.WPF.View.Modes.Settings;
+
+public partial class TargetPIDOption : UserControl
+{
+    public TargetPIDOption()
+    {
+        InitializeComponent();
+    }
+}
