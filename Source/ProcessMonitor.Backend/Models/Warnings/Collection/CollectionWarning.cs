c0d89f65fc83815b735eedfa60c57723f1a9b913
serdekh <mistersergey055@gmail.com>
2026-09-10 21:09:46 +0200

    Add warning derivatives for Collection layer

---
+++ Source/ProcessMonitor.Backend/Models/Warnings/Collection/CollectionWarning.cs
@@ -0,0 +1,5 @@
+using ProcessMonitor.Shared.Models.Results;
+
+namespace ProcessMonitor.Backend.Models.Warnings.Collection;
+
+public abstract record CollectionWarning : Warning;
---
+++ Source/ProcessMonitor.Backend/Models/Warnings/Collection/ProcessDoesNotExist.cs
@@ -0,0 +1,9 @@
+namespace ProcessMonitor.Backend.Models.Warnings.Collection;
+
+public record ProcessDoesNotExist(int ProcessId) : CollectionWarning
+{
+    public override string ToString()
+    {
+        return $"Process {ProcessId} does not exist.";
+    }
+}
