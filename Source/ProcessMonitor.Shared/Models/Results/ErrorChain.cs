3f3a9f234d2828e7d6b6bf4eef3261acea7822e3
serdekh <mistersergey055@gmail.com>
2026-09-10 20:25:40 +0200

    Make chain nullable

--- Source/ProcessMonitor.Shared/Models/Results/ErrorChain.cs
+++ Source/ProcessMonitor.Shared/Models/Results/ErrorChain.cs
@@ -2,7 +2,7 @@ namespace ProcessMonitor.Shared.Models.Results;
 
 public sealed record ErrorChain<TError>(
     TError Error,
-    ErrorChain<TError> Inner) 
+    ErrorChain<TError>? Inner) 
     where TError : Error
 {
     public ErrorChain<TError> CausedBy(TError error) => new(error, this);
