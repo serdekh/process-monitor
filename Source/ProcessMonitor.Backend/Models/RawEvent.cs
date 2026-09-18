382c3f4a4b0f324d3ec292edea442f7cd3189b9a
serdekh <mistersergey055@gmail.com>
2026-09-18 17:57:41 +0200

    Make raw event source nullable for tests

--- Source/ProcessMonitor.Backend/Models/RawEvent.cs
+++ Source/ProcessMonitor.Backend/Models/RawEvent.cs
@@ -2,9 +2,9 @@ using Microsoft.Diagnostics.Tracing;
 
 namespace ProcessMonitor.Backend.Models;
 
-public struct RawEvent(TraceEvent e, RawEventKind kind)
+public struct RawEvent(TraceEvent? e, RawEventKind kind)
 {
-    public TraceEvent Source { get; set; } = e.Clone();
+    public TraceEvent? Source { get; set; } = e?.Clone();
 
     public RawEventKind Kind { get; set; } = kind;
 }
