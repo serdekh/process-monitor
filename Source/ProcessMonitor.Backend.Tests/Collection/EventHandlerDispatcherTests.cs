1f81af75681daac1a03aa3d4423286580e339bae
serdekh <mistersergey055@gmail.com>
2026-09-19 18:54:21 +0200

    Add a test case for DispatchEvent method

--- Source/ProcessMonitor.Backend.Tests/Collection/EventHandlerDispatcherTests.cs
+++ Source/ProcessMonitor.Backend.Tests/Collection/EventHandlerDispatcherTests.cs
@@ -1,6 +1,66 @@
+using System.Threading.Channels;
+using Microsoft.Diagnostics.Tracing.Parsers.MicrosoftWindowsTCPIP;
+using Moq;
+using ProcessMonitor.Backend.Collection;
+using ProcessMonitor.Backend.Models;
+using ProcessMonitor.Backend.Models.Collection;
+using ProcessMonitor.Backend.Models.Errors.Collection;
+using ProcessMonitor.Backend.Models.Warnings.Collection;
+using ProcessMonitor.Backend.State;
+using ProcessMonitor.Shared.Models;
+using ProcessMonitor.Shared.Models.Results;
+
 namespace ProcessMonitor.Backend.Tests.Collection;
 
 public class EventHandlerDispatcherTests
 {
-    
+    // TODO: Add tests for 
+        //DispatchEvent;
+        //HandleEvent
+        //HandleThreadStart(TraceEvent data);
+        //HandleThreadDCStart(TraceEvent data);
+        //HandleThreadStop(TraceEvent data);
+        //HandleThreadDCEnd(TraceEvent data);
+        //HandleContextSwitch(TraceEvent data);
+        //HandleSyscallEnter(TraceEvent data);    
+        //HandleUndefined(TraceEvent data);
+
+    // private Channel<RawEvent> _input;
+    // private MonitoringSessionState _state;
+    // private EventCollectorContext _ctx;
+
+    // public EventHandlerDispatcherTests()
+    // {
+    //     _input = Channel.CreateUnbounded<RawEvent>();
+    //     _state = new MonitoringSessionState(42);
+    //     _ctx = new EventCollectorContext(_input, _state);
+    // }
+
+    [Fact]
+    public void DispatchEvent_ReturnsFailure_WhenSeedingExistingThreadsFails()
+    {
+        // Arrange
+        var expectedProcessId = 42;
+
+        var fakeEvent = new Mock<ITraceEvent>();
+        var fakeCtx = new Mock<IEventCollectorContext>();
+        var fakeCtxFailure = 
+            new Failure<None, CollectionError, CollectionWarning>(
+                new ErrorChain<CollectionError>(
+                    new ProcessIdUpdateFailed(), null));
+
+        fakeCtx.Setup(e => e.ProcessId).Returns(expectedProcessId);
+        fakeCtx.Setup(e => e.TryUpdateTargetProcess()).Returns(fakeCtxFailure);
+        
+        var dispatcher = new EventHandlerDispatcher(fakeCtx.Object);
+
+        // Act
+        var result = dispatcher.DispatchEvent(fakeEvent.Object);
+
+        // Assert
+        Assert.True(result is Failure<None, CollectionError, CollectionWarning>);
+
+        var failure = (Failure<None, CollectionError, CollectionWarning>)result;
+        Assert.True(failure.Chain.Error is EventDispatchingFailed);
+    }
 }
