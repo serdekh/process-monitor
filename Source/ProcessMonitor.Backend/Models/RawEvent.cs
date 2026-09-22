e0162573d3b7022a19cfa909577e2b6d4d2f7967
serdekh <mistersergey055@gmail.com>
2026-09-22 20:23:24 +0200

    Push RawEvent closer to removing the Source member

--- Source/ProcessMonitor.Backend/Models/RawEvent.cs
+++ Source/ProcessMonitor.Backend/Models/RawEvent.cs
@@ -4,7 +4,11 @@ namespace ProcessMonitor.Backend.Models;
 
 public struct RawEvent(TraceEvent? e, RawEventKind kind)
 {
-    public TraceEvent? Source { get; set; } = e?.Clone();
+    public bool HasSource { get; init; } = e is null;
+
+    public TraceEvent? Source { get; set; } = e;
+
+    public double TimeStampRelativeMSec { get; set; } = e?.TimeStampRelativeMSec ?? 0;
 
     public RawEventKind Kind { get; set; } = kind;
 }
